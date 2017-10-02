86ea79dae812ddf2d4a925da5c827530773b6858
Omar <silen08620@gmail.com>
2017-10-02 18:27:21 +0200

    - New project

---
+++ stockfish-2-2.13-ios-src/CaptureSessionManager/CaptureSessionManager.h
@@ -0,0 +1,94 @@
+//
+//  CaptureSessionManager.h
+//  
+//
+//  Created by silenGSR on 5/11/15.
+//  Copyright © 2015 Nibble development. All rights reserved.
+//
+
+
+#define kFlashAuto 0
+#define kFlashOn 1
+#define kFlashOff 2
+
+
+
+
+#import <Foundation/Foundation.h>
+
+#import <CoreMedia/CoreMedia.h>
+#import <AVFoundation/AVFoundation.h>
+
+@protocol CaptureSessionManagerDelegate <NSObject>
+
+
+- (void) CaptureSessionManagerDelegate_PhotoTaked:(UIImage*) photo;
+- (void) CaptureSessionManagerDelegate_PhotoFailed;
+- (void) CaptureSessionManagerDelegate_EndProcessCIImage;
+- (void) showNotAuthorizeAudio:(BOOL) isAudio isCamera:(BOOL) isCamera isGalery:(BOOL) isGalery;
+
+- (void) CaptureSessionManagerDelegate_DidFinishVideoRecording:(NSURL*) url;
+- (void) CaptureSessionStartTimerVideo;
+
+@end
+
+@interface CaptureSessionManager : NSObject <
+AVCaptureFileOutputRecordingDelegate,
+AVCaptureAudioDataOutputSampleBufferDelegate,
+AVCaptureVideoDataOutputSampleBufferDelegate>
+
+@property (nonatomic, weak) id <CaptureSessionManagerDelegate> delegate;
+
+@property UIDeviceOrientation deviceOrientation;
+@property BOOL isRunning;
+@property NSInteger flashMode;
+
+@property (retain) AVCaptureVideoPreviewLayer *previewLayer;
+@property (retain) AVCaptureSession *captureSession;
+@property (retain) AVCaptureDevice *videoDevice;
+@property (retain) AVCaptureDeviceInput *videoInput;
+@property (retain) AVCaptureDevice *audioCapture;
+
+@property (retain) AVCaptureDeviceInput * audioInput;
+
+//audioDevice = [AVCaptureDevice
+@property (retain) AVCaptureStillImageOutput *output;
+
+@property (retain) AVCaptureVideoDataOutput *videoDataOutput;
+@property (retain) AVCaptureMovieFileOutput *movieFileOutput;
+@property (assign) UIImageOrientation orientationLastPhoto;
+@property (retain) UIImage *lastPhotoTaked;
+@property (retain) CIImage *lastPhotoCI;
+
+@property (retain) UIImage *lastPhotoTakedSmall;
+@property (retain) CIImage *lastPhotoCISmall;
+
+@property BOOL isSaveVideo;
+@property (retain) NSURL *urlLastVideo;
+@property UIImage *imgVideoThumbnail;
+
+@property BOOL isFrontCamera;
+
+- (id)initWithFrame:(CGRect)frame withDel:(id<CaptureSessionManagerDelegate>)del;
+- (id)initWithFrame:(CGRect)frame;
+- (void) isAvailableAuthorization;
+- (CIImage*) getCIImageForFilter;
+- (CIImage*) getCIImageForFilterSmall;
+
+- (void)takePhoto;
+- (void)toggleCam;
+- (void)startRunning;
+- (void)stopRunning;
+
+- (void) generateAndSaveInternalImagesAfterTakePhotoWithPhoto:(UIImage*) photo;
+- (void) tapToFocus:(UITapGestureRecognizer* ) singleTap inContainerView:(UIView*) containerView;
+
+//- (void) beginRecord;
+- (void) stopVideoRecord;
+
+- (void) prepareForvideo;
+- (void) prepareForPhoto;
+
+- (void) resetPhoto;
+
+@end
