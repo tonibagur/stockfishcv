c1da3521d590a49f1ef3392aa657146d203c6b6d
Omar <silen08620@gmail.com>
2017-10-17 14:09:54 +0200

    - Begin PythonManager

---
+++ stockPython/try_php2/Managers/PythonManager/PythonManager.h
@@ -0,0 +1,13 @@
+//
+//  PythonManager.h
+//  stockPython
+//
+//  Created by Omar on 17/10/17.
+//  Copyright © 2017 coneptum. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PythonManager : NSObject
+
+@end
---
+++ stockPython/try_php2/Managers/PythonManager/PythonManager.m
@@ -0,0 +1,13 @@
+//
+//  PythonManager.m
+//  stockPython
+//
+//  Created by Omar on 17/10/17.
+//  Copyright © 2017 coneptum. All rights reserved.
+//
+
+#import "PythonManager.h"
+
+@implementation PythonManager
+
+@end
